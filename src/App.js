04c08fbe92d83560f641b49cc5ac265416af8d58
taviishii <tavishi.9905@gmail.com>
2024-04-09 21:33:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import {Reviews} from "./MyComponents/Reviews";
+import {Footer} from "./MyComponents/Footer";
 
 function App() {
   
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">BarterBounty</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Reviews</a>
-        </li>
-        
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+    <Header title="BarterBounty"/>
+    <Reviews/>
+    <Footer/>
     </>
   );
 }
